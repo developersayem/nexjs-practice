bf68af66f989ed209283981157661d1465ecccda
devsayemmolla <98878071+devsayemmolla@users.noreply.github.com>
2022-12-16 00:08:20 +0600

    routing in next js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 const index = () => {
   return (
     <div>
-
+      <h1>
+        <Link href='/users'>
+          Users
+        </Link>
+      </h1>
     </div>
   );
 }
--- pages/users/index.js
+++ pages/users/index.js
@@ -2,7 +2,7 @@ import User from "../../components/User/User";
 
 const users = ({ users }) => {
     return (
-        <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-16">
             {
                 users.map(user => <User key={user.id} user={user}></User>)
             }
